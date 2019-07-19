f492b753827730c5f9ce73c5f64d306008ba109c
imad-ramy <turlax800@outlook.fr>
2019-07-19 11:20:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+    
+export default class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    componentDidMount(){
+      setTimeout(function(){ alert("Hello messqge"); }, 2000);
 
-export default App;
+      }
+
+      componentWillUnmount(){
+        alert("goodbye")
+      }
+
+render(){
+      return (<div><h3>Hello</h3></div>)
+    }
+
+};
+    
+
+
